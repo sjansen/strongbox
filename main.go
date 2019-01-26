6fc3a7e2ba65f6c072c643a7e06c0ebad17a513f
Stuart Jansen <sjansen@buscaluz.org>
2019-01-26 12:30:28 -0700

    add randomized quotes to API

--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+var quotes = []string{
+	"Despite all my rage I am still just a rat in a cage ",
+	"I get knocked down, but I get up again.",
+	"I'm the Scatman!",
+	"If you want to destroy my sweater, hold this thread as I walk away.",
+	"Kilroy was here.",
+	"Make a little birdhouse in your soul.",
+	"Movin' to the country, gonna eat a lot of peaches.",
+	"Soy un perdedor.",
+	"Spoon!",
+	"The world is a vampire.",
+}
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func handler(req events.APIGatewayProxyRequest) (resp events.APIGatewayProxyResponse, err error) {
 	fmt.Printf("x-amzn-trace-id=%q.\n", req.Headers["x-amzn-trace-id"])
 
+	body, err := json.Marshal(map[string]string{
+		"quote": quotes[rand.Intn(len(quotes))],
+	})
+	if err != nil {
+		resp.StatusCode = 500
+		resp.Headers = map[string]string{
+			"Cache-Control": "no-cache, no-store, must-revalidate",
+			"Content-Type":  "text/text; charset=utf-8",
+		}
+		resp.Body = fmt.Sprintf("x-amzn-trace-id=%q.\n", req.Headers["x-amzn-trace-id"])
+		return
+	}
+
 	resp.StatusCode = 200
-	resp.Headers = map[string]string{"Content-Type": "application/json; charset=utf-8"}
-	resp.Body = `{"quote": "Spoon!"}`
+	resp.Headers = map[string]string{
+		"Cache-Control": "max-age=15",
+		"Content-Type":  "application/json; charset=utf-8",
+	}
+	resp.Body = string(body)
 	return
 }
 
